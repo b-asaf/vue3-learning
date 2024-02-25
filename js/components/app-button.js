4d726574094e6102aa5f7a004cbbbea8eab28319
asaf bar <assafbar81@gmail.com>
2024-02-25 11:42:34 +0200

    Components props, making them more flexible

--- js/components/app-button.js
+++ js/components/app-button.js
@@ -1,13 +1,33 @@
 export default {
   template: `
-    <button class="bg-gray-200 hover:bg-gray-400 border rounded-md px-5 py-2 disabled:cursor-not-allowed" :disabled="processing">
-        <slot />
-    </button>
+  <button
+    :class="{
+      'border rounded-md px-5 py-2 disabled:cursor-not-allowed': true,
+      'bg-blue-200 hover:bg-blue-400': type === 'primary',
+      'bg-purple-400 hover:bg-purple-200': type ==='secondary',
+      'bg-gray-200 hover:bg-gray-400': type === 'muted',
+      'is-loading': processing,
+    }"
+    :disabled="processing"
+  >
+      <slot />
+  </button>
   `,
 
-  data() {
-    return {
-      processing: true,
-    };
+  props: {
+    type: {
+      type: String,
+      default: "primary",
+    },
+
+    processing: {
+      type: Boolean,
+      default: false,
+    },
   },
+  // data() {
+  //   return {
+  //     processing: false,
+  //   };
+  // },
 };
---
+++ js/components/app.js
@@ -0,0 +1,8 @@
+import appButton from "./app-button.js";
+
+export default {
+  // register the component so it can be used in the current component (in this case app.js component)
+  components: {
+    "app-button": appButton,
+  },
+};
