2148f1bffda035e1174bcda8fefbd6e3d00ff7da
asaf bar <assafbar81@gmail.com>
2024-02-26 11:27:31 +0200

    mixins & composables: extract common logic to external file and use 2 approaches to handle it

--- vue-project/src/views/AboutView.vue
+++ vue-project/src/views/AboutView.vue
@@ -1,15 +1,30 @@
-<template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
-</template>
+<script setup>
+// // Approach A: use mixins for common functionality
+// import flash from '@/mixins/flash'
 
-<style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
-}
-</style>
+// export default {
+//   mixins: [flash]
+// }
+
+// // Approach B: use composable (similar to hooks in React) for common functionality
+// import { useFlash } from '@/composables/useFlash'
+
+// export default {
+//   setup() {
+//     let { flash } = useFlash()
+
+//     return { flash }
+//   }
+// }
+
+// Approach C: move setup object to be an attribute in the script tag
+import { useFlash } from '@/composables/useFlash'
+
+let { flash } = useFlash()
+</script>
+
+<template>
+  <p>
+    <button @click="flash('Yay', 'it worked on the about page', 'info')">Click me</button>
+  </p>
+</template>
--- vue-project/src/views/HomeView.vue
+++ vue-project/src/views/HomeView.vue
@@ -1,9 +1,32 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+// // Approach A: use mixins for common functionality
+// import flash from '@/mixins/flash'
+
+// export default {
+//   mixins: [flash]
+// }
+
+// // Approach B: use composable (similar to hooks in React) for common functionality
+// import { useFlash } from '@/composables/useFlash'
+
+// export default {
+//   setup() {
+//     let { flash } = useFlash()
+
+//     return { flash }
+//   }
+// }
+
+// Approach C: move setup object to be an attribute in the script tag
+import { useFlash } from '@/composables/useFlash'
+
+let { flash } = useFlash()
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <p>
+      <button @click="flash('Test', 'it worked')">Click me</button>
+    </p>
   </main>
 </template>
