2e64ccf4cf4abb876f9d27b2d63d77549c5d40c8
asaf bar <assafbar81@gmail.com>
2024-02-29 15:30:59 +0200

    create a simple Modal component

---
+++ vue-project/src/components/Modal.vue
@@ -0,0 +1,54 @@
+<script setup>
+defineProps({
+  show: Boolean
+})
+</script>
+
+<template>
+  <div v-if="show" class="modal-mask" @click="$emit('close')">
+    <div class="modal-inner">
+      <header><slot name="header">default header</slot></header>
+
+      <div><slot>default body</slot></div>
+
+      <footer class="modal-footer">
+        <slot name="footer"><button @click="$emit('close')">Default Close</button></slot>
+      </footer>
+    </div>
+  </div>
+</template>
+
+<style>
+.modal-mask {
+  position: fixed;
+  inset: 0;
+  background: rgba(0, 0, 0, 0.6);
+  display: grid;
+  place-items: center;
+  color: black;
+}
+.modal-inner {
+  background: white;
+  padding: 1rem;
+  width: 80vw;
+  max-width: 500px;
+  border-radius: 8px;
+}
+
+.modal-footer {
+  border-top: 1px solid #ddd;
+  margin-top: 1rem;
+  padding-top: 0.5rem;
+  font-size: 0.8rem;
+}
+
+.modal-footer button {
+  background: #ddd;
+  padding: 0.25rem 0.75rem;
+  border-radius: 20px;
+
+  &:hover {
+    background: #c8c8c8;
+  }
+}
+</style>
